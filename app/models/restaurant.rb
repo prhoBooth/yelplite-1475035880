f01e132553d1f23bd0375e0d92426173a45a4655
Raghu Betina <raghu@firstdraft.com>
2016-09-28 04:14:26 +0000

    Direct association between restaurants and owners

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :owners,
+             :dependent => :destroy
+
   has_many   :photos,
              :dependent => :destroy
 
